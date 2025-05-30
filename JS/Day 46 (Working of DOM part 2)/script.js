ea8ae5f04aa1a50b1c88d0671bedf53672d8f70d
Tirth9978 <tirthppatel9978@gmail.com>
2025-05-30 10:44:34 +0530

    I add more code

---
+++ JS/Day 46 (Working of DOM part 2)/script.js
@@ -0,0 +1,13 @@
+
+// Select elemnet by calss name
+let a = document.getElementsByClassName("box")
+
+console.log(a)
+a[2].style.background = "red";
+a[2].style.color = "white"
+
+
+// Get element by Id 
+let b = document.getElementById("blue")
+b.style.background = "blue"
+b.style.color = "white"
