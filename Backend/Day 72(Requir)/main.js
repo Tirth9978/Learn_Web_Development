8bc1d881cfa291a73c61af93c92c9a9fb838199a
Tirth9978 <tirthppatel9978@gmail.com>
2025-06-17 21:47:57 +0530

    I add more in the BackEnd

--- Backend/Day 72(Requir)/main.js
+++ Backend/Day 72(Requir)/main.js
@@ -2,11 +2,14 @@ console.log("I am Runing:)")
 
 const fs = require("fs");
 const os = require("os");
+const New = require("./new.js")
 
-console.log(os.userInfo().username)
+console.log(os.userInfo())
 
 fs.appendFile("Tirth.txt",`Hello ${os.userInfo().username}\n` ,(err)=>{
      if (err){
           console.log("File is not Exist:(")
      }
 })
+
+console.log(New.age)
---
+++ Backend/Day 72(Requir)/new.js
@@ -0,0 +1,5 @@
+console.log("New.js is Staring now :)")
+
+module.exports.age = 19
+
+// console.log(module.exports)
