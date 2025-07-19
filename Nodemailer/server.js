a85fd9b243c329e43a39ea75607482de32e75548
Tirth9978 <tirthppatel9978@gmail.com>
2025-07-19 13:05:38 +0530

    I add the Nodeemailer

---
+++ Nodemailer/server.js
@@ -0,0 +1,24 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+     service: 'gmail',
+     auth: {
+          user: 'tirthppatel9999@gmail.com',
+          pass: 'fjknwtuecgzdwycf'
+     }
+});
+
+const mailOptions = {
+     from: 'tirthppatel9999@gmail.com',
+     to: 'tirthppatel9978@gmail.com',
+     subject: 'Your OTP Code',
+     text: `Your OTP is 1234`
+};
+
+transporter.sendMail(mailOptions, function (error, info) {
+     if (error) {
+          console.log(error);
+     } else {
+          console.log('Email sent: ' + info.response);
+     }
+});
